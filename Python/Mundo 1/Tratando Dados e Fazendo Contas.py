411933436c8a027c9968fa2154614839a991b36c
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-04-19 10:47:11 +0000

    Exercícios dos primeiros dois capítulos.

---
+++ Python/Mundo 1/Tratando Dados e Fazendo Contas.py
@@ -0,0 +1,58 @@
+# Exercício 4
+'''
+n=input("Digita algo:")
+print(f"O tipo primitivo é {type(n)}")
+print(f"É número? {n.isnumeric()}")
+print(f"É letra? {n.isalpha()}")
+print(f"É número com letra? {n.isalnum()}")'''
+
+# Exercício 5
+'''u=int(input("Digita um número:"))
+print(f"O seu antecessor é {u-1} e o seu sucessor é {u+1}.")'''
+
+# Execício 6
+'''u=int(input("Digita um número:"))
+print(f"O seu dobro é {u*2}; \nO seu triplo é {u*3}; \nA sua raiz quadrada é {u**0.5:.0f};")'''
+
+#Exercício 7
+'''a,b=int(input("Digita a primeira nota:")), int(input("Digita a Segunda nota:"))
+print(f"A média é {(a+b)/2}!")'''
+
+#Exercício 8
+'''a=int(input("Digita o valor em metros:"))
+print(f"{a*100}cm;\n{a*1000}mm;")'''
+
+# Exercício 9
+'''a=int(input("Digita um número:"))
+print(f"{a} X 1 = {a*1}\n"
+      f"{a} X 2 = {a*2}\n"
+      f"{a} X 3 = {a*3}\n"
+      f"{a} X 4 = {a*4}\n"
+      f"{a} X 5 = {a*5}\n"
+      f"{a} X 6 = {a*6}\n"
+      f"{a} X 7 = {a*7}\n"
+      f"{a} X 8 = {a*8}\n"
+      f"{a} X 9 = {a*9}\n"
+      f"{a} X 10 = {a*10}\n"
+      f"{a} X 11 = {a*11}\n"
+      f"{a} X 12 = {a*12}")'''
+
+# Exercício 10
+'''a=int(input("Quantos euros tens? :"))
+print(f"Podes trocar para {a*1.08689:.2f}$.")'''
+
+# Exercício 11
+'''a,b=int(input("Digita o comprimento da parede:")), int(input("Digita a largura da parede:"))
+print(f"Área da parede é de {a*b}m2 e será necessário {a*b/2} litros de tinta.")'''
+
+# Exercício 12
+'''a=int(input("Digita o preço:"))
+print(f"Com 5% de desconto, o preço será {a-a/20:.1f}!")'''
+
+# Exercício 13
+'''a=int(input("Digita o salário:"))
+print(f"Com 15% de aumento, o salário será {a+a*0.15} !")'''
+
+# Exercício 14
+
+# Exercício 15

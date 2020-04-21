1ec0e65fc8eacf5542677507c74be62867bb4f92
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-04-21 16:36:21 +0000

    exercícios de modulos

--- Python/Mundo 1/Usando módulos do python.py
+++ Python/Mundo 1/Usando módulos do python.py
@@ -24,3 +24,35 @@ print(f"O aluno escolhido foi {choice([input('Primeiro aluno:'),input('Segundo a
 a=[input("Primeiro aluno:"),input("Segundo aluno:"),input("Terceiro aluno:"),input("Quarto aluno:")]
 shuffle(a)# O shuffe tem de ser feito SOZINHO.
 print(a)'''
+
+# Exercício 22
+'''a=input("Digita o teu nome completo:")
+print(a.upper())
+print(a.lower())
+print(f"O nome {a} tem {len(a)-a.count(' ')} letras.")
+print(f"O nome {a.split()[0]} tem {len(a.split()[0])} letras.")'''
+
+# Exercício 23
+'''a=int(input("Digita um número de 0 a 9999:"))
+print(f"Unidade:{a%10}\n"
+      f"Dezena:{a//10%10}\n"
+      f"Centena:{a//100%10}\n"
+      f"Milhar:{a//1000%10}")'''
+
+# Exercício 24
+'''a=input("Digita o nome de uma cidade e descobriremos se começa com Santo:")
+print("santo" in a.lower().split()[0])'''
+
+# Exercício 25
+'''a=input("Digita um nome e descobriremos se o nome leva silva:")
+print("silva" in a.lower())'''
+
+# Exercício 26
+'''a=input("Digita uma frase:").strip().lower()
+print(f"A letra (A) aparece {a.count('a')} vezes.")
+print(f"Aparece pela primeira vez na posição {a.find('a')+1}")
+print(f"Aparece pela ultima vez na posicção {a.rfind('a')+1}")
+'''
+# Exercício 27
+'''b=input("Digita o teu nome completo:")
+print(f"Primeiro nome:{b.split()[0]}.\nUltimo nome:{b.split()[-1]}.")'''

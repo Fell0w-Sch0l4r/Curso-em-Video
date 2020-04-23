e6df664969f0dcd390254d668297e8ed473babd7
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-04-23 12:23:16 +0000

    Adicionei Cores
    Depois de aprender o módulo de cores, adicionei-as no ficheiro "3.Usando módulos em python,py"

--- Python/Mundo 1/3.Usando módulos do python.py
+++ Python/Mundo 1/3.Usando módulos do python.py
@@ -1,58 +1,69 @@
 # Exercício 16
 '''from math import floor
-print(floor(float(input("Digita um número:"))))'''
+a=float(input("\033[4:32mDigita um número:\033[m"))
+print(f'\033[1:31m{floor(a)}\033[m')'''
 
 # Exercício 17
 '''from math import hypot
-print(f"O valor da hipotenusa é "
-      f"{hypot(int(input('Cateto Adjacente:')),int(input('Cateto Oposto:')))}")'''
+hip=hypot(int(input('\033[4:34mCateto Adjacente:\033[m')),int(input('\033[4:32mCateto Oposto:\033[m')))
+print(f"\033[1:33mO valor da hipotenusa é "
+      f"{hip}\033[m")'''
 
 # Exercício 18
 # Essa função de trigonometria apenas lê em radianos e não em graus, logo tive de converter o ângulo digitado em radiano.
 '''from math import sin,cos,tan,radians
-a=radians(float(input("Digita um ângulo:")))
-print(f"Cosseno é: {cos(a):.1f} !\n"
-      f"Seno é {sin(a):.1f} !\n"
-      f"Tangente é {tan(a):.1f} !")'''
+a=radians(float(input("\033[0:31mDigita um ângulo:\033[m")))
+print(f"\033[4:32mCosseno é: {cos(a):.1f} !\n\033[m"
+      f"\033[4:33mSeno é {sin(a):.1f} !\n\033[m"
+      f"\033[1:34mTangente é {tan(a):.1f} !\033[m")'''
 
 # Exercício 19
 '''from random import choice
-print(f"O aluno escolhido foi {choice([input('Primeiro aluno:'),input('Segundo aluno:'),input('Terceiro aluno:'),input('Quarto aluno:')])}!")
-'''
+print("\033[1:31mO aluno escolhido foi"
+      "\033[m \033[4:32m{}\033[m!"
+      .format(choice([input('\033[4:33mPrimeiro aluno:\033[m'),
+                      input('\033[4:34mSegundo aluno:\033[m'),
+                      input('\033[4:35mTerceiro aluno:\033[m'),
+                      input('\033[4:36mQuarto aluno:\033[m')])))'''
+
 # Exercício 20
 '''from random import shuffle
-a=[input("Primeiro aluno:"),input("Segundo aluno:"),input("Terceiro aluno:"),input("Quarto aluno:")]
-shuffle(a)# O shuffe tem de ser feito SOZINHO.
+a=[input("\033[4:32mPrimeiro aluno:\033[m"),
+   input("\033[4:33mSegundo aluno:\033[m"),
+   input("\033[4:34mTerceiro aluno:\033[m"),
+   input("\033[4:35mQuarto aluno:\033[m")]
+shuffle(a)# O shuffe tem de ser feito ((("SOZINHO"))).
 print(a)'''
 
 # Exercício 22
-'''a=input("Digita o teu nome completo:")
-print(a.upper())
-print(a.lower())
-print(f"O nome {a} tem {len(a)-a.count(' ')} letras.")
-print(f"O nome {a.split()[0]} tem {len(a.split()[0])} letras.")'''
+'''a=input("\033[4:31mDigita o teu nome completo:\033[m")
+print(f"\033[1:32m{a.upper()}\033[m")
+print(f"\033[1:33m{a.lower()}\033[m")
+print(f"\033[1:34mO nome {a} tem {len(a)-a.count(' ')} letras.\033[m")
+print(f"\033[1:35mO nome {a.split()[0]} tem {len(a.split()[0])} letras.\033[m")'''
 
 # Exercício 23
-'''a=int(input("Digita um número de 0 a 9999:"))
-print(f"Unidade:{a%10}\n"
-      f"Dezena:{a//10%10}\n"
-      f"Centena:{a//100%10}\n"
-      f"Milhar:{a//1000%10}")'''
+'''a=int(input("\033[4:31mDigita um número de 0 a 9999:\033[m"))
+print(f"\033[1:32mUnidade:{a%10}\n\033[m"
+      f"\033[1:33mDezena:{a//10%10}\n\033[m"
+      f"\033[1:34mCentena:{a//100%10}\n\033[m"
+      f"\033[1:35mMilhar:{a//1000%10}\033[m")'''
 
 # Exercício 24
-'''a=input("Digita o nome de uma cidade e descobriremos se começa com Santo:")
-print("santo" in a.lower().split()[0])'''
+'''a=input("\033[4:33mDigita o nome de uma cidade e descobriremos se começa com Santo:\033[m")
+print(f"\033[1:31m{'santo' in a.lower().split()[0]}\033[m")'''
 
 # Exercício 25
-'''a=input("Digita um nome e descobriremos se o nome leva silva:")
-print("silva" in a.lower())'''
+'''a=input("\033[4:32mDigita um nome e descobriremos se o nome leva silva:\033[m")
+print(f"\033[1:31m{'silva' in a.lower()}\033[m")'''
 
 # Exercício 26
-'''a=input("Digita uma Frase:").strip().lower()
-print(f"A letra (A) aparece {a.count('a')} vezes.")
-print(f"Aparece pela primeira vez na posição {a.find('a')+1}")
-print(f"Aparece pela ultima vez na posicção {a.rfind('a')+1}")
-'''
+'''a=input("\033[4:31mDigita uma Frase:\033[m").strip().lower()
+print(f"\033[1:32mA letra (A) aparece {a.count('a')} vezes.\033[m")
+print(f"\033[1:34mAparece pela primeira vez na posição {a.find('a')+1}\033[m")
+print(f"\033[1:35mAparece pela ultima vez na posicção {a.rfind('a')+1}\033[m")'''
+
 # Exercício 27
-'''b=input("Digita o teu nome completo:")
-print(f"Primeiro nome:{b.split()[0]}.\nUltimo nome:{b.split()[-1]}.")'''
+'''b=input("\033[4:31mDigita o teu nome completo:\033[m")
+print(f"\033[1:34mPrimeiro nome:{b.split()[0]}.\n\033[m"
+      f"\033[1:35mUltimo nome:{b.split()[-1]}.\033[m")'''

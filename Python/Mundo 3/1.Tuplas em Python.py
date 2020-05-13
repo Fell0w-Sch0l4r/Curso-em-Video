89eea21b66cf63b2d8bfca88991984a7d45269e4
fell0w-sch0l4r <angelcagiza@gmail.com>
2020-05-13 23:50:37 +0100

    exercícios de tupla feitos

--- Python/Mundo 3/1.Tuplas em Python.py
+++ Python/Mundo 3/1.Tuplas em Python.py
@@ -21,16 +21,14 @@ print(f"Os últimos 4 são: {brasileirao[-4:]}")
 print(f"As equipas em ordem alfabética:{sorted(brasileirao)}")
 print(f"O Chapecoense está na {brasileirao.index('Chapecoense')}ª posição.")'''
 # Exercício 74
-from random import randint
+'''from random import randint
 numeros=(randint(0,10),randint(0,10),randint(0,10),randint(0,10),randint(0,10))
 print("Os valores sorteados foram:",end=" ")
 maior=menor=numeros[0]
 for c in numeros:
     print(c,end=" ")
-print(f"\nO maior número é o {max(numeros)} é o menor o {min(numeros)}.")
+print(f"\nO maior número é o {max(numeros)} é o menor o {min(numeros)}.")'''
 # Exercício 75
-
-
 '''valores=(int(input("Primero vloer:")),int(input("Segundo vlor:")),
          int(input("Terceiro valor:")),int(input("Quarto valor:")))
 print(valores)
@@ -40,22 +38,27 @@ if 3 in valores:
 else:
     print("O valor 3 não foi digitado em nenhuma posição.")
 print("Os vaalores pares digitados foram:",end=" ")
-p=0
 for c in valores:
     if c%2==0:
-        print(c,end=" ")
-    else:
-        p+=1'''
+        print(c,end=" ")'''
 # Exercício 76
-'''listagem=("Lápis",1.76,"Borracha",2,"Caderno",15.90,"Estojo",25)
+'''listagem=("Lápis",1.76,
+          "Borracha",2,
+          "Caderno",15.90,
+          "Estojo",25,
+          "Transferidor",4.20,
+          "Compasso",9.99,
+          "Mochila",120.32,
+          "Canetas",22.30,
+          "Livro",34.90)
 print("-"*40)
-print("           LISTAGEM DE PREÇOS")
+print(f"{'LISTAGEM DE PREÇOS':^40}")
 print("-"*40)
 for c,m in enumerate(listagem):
     if c%2==0:
         print(f"{m:.<30}R$",end="")
     else:
-        print(f"{m:>7}")
+        print(f"{m:>7.2f}") #MEMORIZA ISSO
 print("-"*40)'''
 # Exercício 77
 '''palavras=("aprender","programar","Linguagem","Python",
@@ -63,6 +66,6 @@ print("-"*40)'''
           "Trabalhar","Mercado","Programador","Futuro")
 for vogais in palavras:
     print(f"\nNa palavra {vogais} temos:",end=" ")
-    for letras in vogais:
+    for letras in vogais.lower():
         if letras in "aeiou":
             print(letras,end=" ")'''
